3e42d79096c4c366406272114337ffd64ac9f1fa
jmatthews64 <jon.matthews64@gmail.com>
2023-10-12 21:00:01 -0400

    Added grabber for display output and commented on written script purpose

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+
+// grabbing all buttons that can be clicked for use in js
 let canClick = document.querySelectorAll(".clickable");
 
-// 
+//grabbing display to output results of clicks
+let calcDisplay = document.querySelector(".display");
 
+//creating click event that allows all buttons to be clicked
 for (let eachButton of canClick) {
     eachButton.addEventListener("click", (event) => {
-        console.log(event);
-        
+
+
     })
 }
